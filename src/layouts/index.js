631b02d51d5d8ae5a9ac194621f111759509c1dc
lixin <lixin544@163.com>
2018-12-17 23:37:31 +0800

    [add]国际化设置为中文

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,19 +2,27 @@
 
 import React, { Component } from 'react';
 import { connect } from 'dva';
+import { LocaleProvider } from 'antd';
+import zh_CN from 'antd/lib/locale-provider/zh_CN';
+import moment from 'moment';
+import 'moment/locale/zh-cn';
 import Layout from '../components/layout/index';
+
+moment.locale('zh-cn');
 
 @connect(({ global }) => ({
   global,
 }))
 class BasicLayout extends Component {
-  componentDidMount(){
+
+  componentDidMount() {
     this.props.dispatch({
       type: 'global/getUserInfo',
     });
   }
-  render () {
-    const {location,children} = this.props;
+
+  renderContainer = () => {
+    const { location, children } = this.props;
     if (location.pathname === '/login') {
       return (
         <div>
@@ -26,6 +34,14 @@ class BasicLayout extends Component {
       <Layout>
         {children}
       </Layout>
+    );
+  }
+  
+  render() {
+    return (
+      <LocaleProvider locale={zh_CN}>
+        {this.renderContainer()}
+      </LocaleProvider>
     );
   }
 }
